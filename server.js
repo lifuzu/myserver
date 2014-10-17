8cf6a9b69723942aadd404593e6f7c06c8090e85
Richard Lee <lifuzu@gmail.com>
2014-10-16 23:31:06 -0700

    Add test suite for chat function.

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ var server = http.listen(app.get('port'), function() {
   debug('Express server listening on port ' + server.address().port);
 });
 
+var clients = {}
 io.on('connection', function(socket) {
+	var userName;
   socket.emit('news', { hello: 'world'});
   socket.on('my other event', function(data) {
     console.log(data);
@@ -18,4 +20,16 @@ io.on('connection', function(socket) {
     console.log('message: ' + msg);
     io.emit('chat message', msg);
   });
+  socket.on('connection name', function(user) {
+    userName = user.name
+    clients[user.name] = socket
+    io.emit('new user', user.name + " has joined.");
+  })
+  socket.on('private message', function(msg) {
+    fromMsg = {from: userName, txt: msg.txt}
+    clients[msg.to].emit('private message', fromMsg)
+  })
+  socket.on('disconnect', function() {
+    delete clients[userName]
+  })
 });
--- test.js
+++ test.js
@@ -1,1 +1,2 @@
 require('./tests/collections');
+require('./tests/chat');
